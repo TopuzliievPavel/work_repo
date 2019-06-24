706da3d8a734d0facb0dad69aad76f864d4cda19
Topuzliiev Pavel <topuzliiev@gmail.com>
2019-06-24 19:42:36 +0300

    Change common.js

--- app/js/common.js
+++ app/js/common.js
@@ -1,57 +1,43 @@
-// jQuery(function(){
-//   initMenu();
-//   hideSpan();
-//   linkDown();
-// });
-//
-// function initMenu() {
-//   var opener = jQuery('.menu-open, .menu-close');
-//   var page = jQuery('html');
-//   var classActive = 'menu-active';
-//
-//   var clickHandler = function(e) {
-//     e.preventDefault();
-//     page.toggleClass(classActive);
-//   };
-//   opener.on('click', clickHandler);
-// }
-//
-// function hideSpan() {
-//   $(".know-more").on("click", function(){
-//     $(this).parent(".visible-text").parent(".services-holder").toggleClass("show");
-//   })
-//   $(".close-wrapp").on("click", function(){
-//     $(this).parent(".hover-text").parent(".services-holder").toggleClass("show");
-//   })
-// }
-//
-// // link down
-// function linkDown() {
-//   $(".navigation a").on("click", function(e) {
-//     if(this.hash !== "") {
-//       e.preventDefault();
-//
-//       const hash = this.hash;
-//
-//       $("html, body").animate({
-//         scrollTop: $(hash).offset().top
-//       }, 1000);
-//     }
-//   });
-// }
-
 jQuery(function(){
   initMenu();
+  linkDown();
 });
 
 function initMenu() {
-  var opener = jQuery('.menu-open');
-  var page = jQuery('html');
-  var classActive = 'menu-active';
+  const opener      = jQuery(".menu-open");
+  const page        = jQuery("html");
+  const classActive = "menu-active";
 
-  var clickHandler = function(e) {
+  const clickHandler = function(e) {
     e.preventDefault();
     page.toggleClass(classActive);
   };
-  opener.on('click', clickHandler);
+  opener.on("click", clickHandler);
+}
+
+//  link down
+function linkDown() {
+  $(".nav a").on("click", function(e) {
+    if(this.hash !== "") {
+      e.preventDefault();
+
+      const hash = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 1000);
+    }
+  });
+}
+
+// add input text
+function initText() {
+  const heading         = document.getElementsByClassName("software__title")[0],
+        textInput        = document.getElementsByClassName("form__input")[0],
+        textInputValue   = textInput.value;
+
+  if (textInputValue == textInputValue) {
+    heading.innerHTML = textInputValue;
+    heading.style.display = "inline-block";
+  }
 }
